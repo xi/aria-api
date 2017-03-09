6ab36503fe3112c1a9b7ef99001a2ea00ba70771
Tobias Bengfort <tobias.bengfort@posteo.de>
2017-03-09 16:16:30 +0100

    init

---
+++ landmark.js
@@ -0,0 +1,263 @@
+var roles = [
+	// abstract
+	'command',
+	'composite',
+	'input',
+	'landmark',
+	'range',
+	'roletype',
+	'section',
+	'sectionhead',
+	'select',
+	'structure',
+	'widget',
+	'window',
+
+	// widget - standalone
+	'alert',
+	'alertdialog',
+	'button',
+	'checkbox',
+	'dialog',
+	'gridcell',
+	'link',
+	'log',
+	'marquee',
+	'menuitem',
+	'menuitemcheckbox',
+	'menuitemradio',
+	'option',
+	'progressbar',
+	'radio',
+	'scrollbar',
+	'slider',
+	'spinbutton',
+	'status',
+	'tab',
+	'tabpanel',
+	'textbox',
+	'timer',
+	'tooltip',
+	'treeitem',
+
+	// widget - composite
+	'combobox',
+	'grid',
+	'listbox',
+	'menu',
+	'menubar',
+	'radiogroup',
+	'tablist',
+	'tree',
+	'treegrid',
+
+	// structure
+	'article',
+	'columnheader',
+	'definition',
+	'directory',
+	'document',
+	'group',
+	'heading',
+	'img',
+	'list',
+	'listitem',
+	'math',
+	'note',
+	'presentation',
+	'region',
+	'row',
+	'rowgroup',
+	'rowheader',
+	'separator',
+	'toolbar',
+
+	// landmark
+	'application',
+	'banner',
+	'complementary',
+	'contentinfo',
+	'form',
+	'main',
+	'navigation',
+	'search',
+];
+
+// https://www.w3.org/TR/html-aria/#docconformance
+var extraSelectors = {
+	article: ['article'],
+	banner: ['body > header'],
+	button: [
+		'button',
+		'input[type="button"]',
+		'input[type="image"]',
+		'input[type="reset"]',
+		'input[type="submit"]',
+		'summary',
+	],
+	cell: ['td'],
+	checkbox: ['input[type="checkbox"]'],
+	columnheader: ['th[scope="col"]'],
+	combobox: [
+		'input[type="email"][list]',
+		'input[type="search"][list]',
+		'input[type="tel"][list]',
+		'input[type="text"][list]',
+		'input[type="url"][list]',
+	],
+	complementary: ['aside'],
+	contentinfo: ['body > footer'],
+	definition: ['dd'],
+	dialog: ['dialog'],
+	document: ['body'],
+	figure: ['figure'],
+	form: ['form'],
+	group: ['details', 'optgroup'],
+	heading: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
+	img: ['img:not[alt=""])'],
+	link: ['a[href]', 'area[href]', 'link[href]'],
+	listbox: ['datalist', 'select'],
+	list: ['dl', 'ol', 'ul'],
+	listitem: ['ul > li', 'ol > li'],
+	main: ['main'],
+	math: ['math'],
+	menuitemcheckbox: ['menuitem[type="checkbox"]'],
+	menuitem: ['menuitem[type="command"]'],
+	menuitemradio: ['menuitem[type="radio"]'],
+	menu: ['menu[type="context"]'],
+	navigation: ['nav'],
+	option: ['option'],
+	progressbar: ['progress'],
+	radio: ['input[type="radio"]'],
+	region: ['section'],
+	rowgroup: ['tbody', 'thead', 'tfoot'],
+	rowheader: ['th[scope="row"]'],
+	row: ['tr'],
+	searchbox: ['input[type="search"]:not([list])'],
+	separator: ['hr'],
+	slider: ['input[type="range"]'],
+	spinbutton: ['input[type="number"]'],
+	status: ['output'],
+	table: ['table'],
+	textbox: [
+		'input[type="email"]:not([list])',
+		'input[type="tel"]:not([list])',
+		'input[type="text"]:not([list])',
+		'input[type="url"]:not([list])',
+		'textarea',
+	],
+};
+
+var inverseHierarchy = {
+	command: ['button', 'link', 'menuitem'],
+	composite: ['grid', 'select', 'tablist'],
+	input: ['checkbox', 'option', 'select', 'spinbutton', 'textbox'],
+	landmark: [
+		'application',
+		'banner',
+		'complementary',
+		'contentinfo',
+		'form',
+		'main',
+		'navigation',
+		'search',
+	],
+	range: ['progressbar', 'spinbutton'],
+	roletype: ['structure', 'widget', 'window'],
+	section: [
+		'definition',
+		'gridcell',
+		'group',
+		'img',
+		'listitem',
+		'marquee',
+		'math',
+		'note',
+		'region',
+		'tooltip',
+	],
+	sectionhead: ['columnheader', 'heading', 'rowheader', 'tab'],
+	select: ['combobox', 'listbox', 'menu', 'radiogroup', 'tree'],
+	structure: [
+		'document',
+		'presentation',
+		'section',
+		'sectionhead',
+		'separator',
+	],
+	widget: [
+		'columnheader',
+		'command',
+		'composite',
+		'gridcell',
+		'input',
+		'range',
+		'row',
+		'rowheader',
+		'tab',
+	],
+	window: ['dialog'],
+	alert: ['alertdialog'],
+	checkbox: ['menuitemcheckbox'],
+	dialog: ['alertdialog'],
+	gridcell: ['columnheader', 'rowheader'],
+	menuitem: ['menuitemcheckbox'],
+	menuitemcheckbox: ['menuitemradio'],
+	option: ['treeitem'],
+	radio: ['menuitemradio'],
+	status: ['timer'],
+	grid: ['treegrid'],
+	menu: ['menubar'],
+	tree: ['treegrid'],
+	directory: ['tablist'],
+	document: ['article'],
+	group: ['row', 'rowgroup', 'select', 'toolbar'],
+	list: ['directory', 'listbox', 'menu'],
+	listitem: ['treeitem'],
+	region: [
+		'alert',
+		'article',
+		'grid',
+		'landmark',
+		'list',
+		'log',
+		'status',
+		'tabpanel',
+	],
+};
+
+var getChildRoles = function(role) {
+	var children = inverseHierarchy[role] || [];
+	var descendents = children.map(getChildRoles);
+
+	var result = [role];
+
+	children.forEach(function(r) {
+		if (result.indexOf(r) == -1) {
+			result.push(r);
+		}
+	});
+	descendents.forEach(function(list) {
+		list.forEach(function(r) {
+			if (result.indexOf(r) == -1) {
+				result.push(r);
+			}
+		});
+	});
+
+	return result;
+};
+
+var _getSelector = function(role) {
+	var selector = '[role="' + role + '"]';
+	var extra = (extraSelectors[role] || []).map(sel => sel + ':not([role])');
+	return [selector].concat(extra).join(',');
+};
+
+var getSelector = function(role) {
+	var roles = getChildRoles(role);
+	return roles.map(_getSelector).join(',');
+};
+
+console.log(getChildRoles('abstract'));
+console.log(getSelector('landmark'));
