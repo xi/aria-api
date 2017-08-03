9a804da5d305a094fb04bf5e34ffc02edc5c1aa4
Tobias Bengfort <tobias.bengfort@posteo.de>
2017-08-03 11:06:06 +0200

    test hidden

---
+++ test/test-hidden.js
@@ -0,0 +1,55 @@
+describe('getAttribute(el, "hidden")', function() {
+	var testbed;
+
+	beforeEach(function() {
+		testbed = document.createElement('div');
+		// make sure styles are actually computed
+		document.body.appendChild(testbed);
+	});
+
+	afterEach(function() {
+		document.body.removeChild(testbed);
+	});
+
+	it('is true on display: none', function() {
+		testbed.innerHTML = '<div class="test" style="display: none">test</div>';
+		var el = testbed.querySelector('.test');
+		expect(aria.getAttribute(el, 'hidden')).toBe(true);
+	});
+
+	it('is true on display: none on parent', function() {
+		testbed.innerHTML = '<div style="display: none"><div class="test">test</div></div>';
+		var el = testbed.querySelector('.test');
+		expect(aria.getAttribute(el, 'hidden')).toBe(true);
+	});
+
+	it('is true on visibility: hidden', function() {
+		testbed.innerHTML = '<div class="test" style="visibility: hidden">test</div>';
+		var el = testbed.querySelector('.test');
+		expect(aria.getAttribute(el, 'hidden')).toBe(true);
+	});
+
+	it('is true on visibility: hidden on parent', function() {
+		testbed.innerHTML = '<div style="visibility: hidden"><div class="test">test</div></div>';
+		var el = testbed.querySelector('.test');
+		expect(aria.getAttribute(el, 'hidden')).toBe(true);
+	});
+
+	it('is false on overwritten visibility: hidden on parent', function() {
+		testbed.innerHTML = '<div style="visibility: hidden"><div class="test" style="visibility: visible">test</div></div>';
+		var el = testbed.querySelector('.test');
+		expect(aria.getAttribute(el, 'hidden')).toBe(false);
+	});
+
+	it('is true on hidden attribute', function() {
+		testbed.innerHTML = '<div class="test" hidden>test</div>';
+		var el = testbed.querySelector('.test');
+		expect(aria.getAttribute(el, 'hidden')).toBe(true);
+	});
+
+	it('is true on hidden attribute on parent', function() {
+		testbed.innerHTML = '<div hidden><div class="test">test</div></div>';
+		var el = testbed.querySelector('.test');
+		expect(aria.getAttribute(el, 'hidden')).toBe(true);
+	});
+});
