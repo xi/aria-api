2866d74c25ec9c4e2f39860f47bb75608d04f3b2
Tobias Bengfort <tobias.bengfort@posteo.de>
2018-02-18 07:57:06 +0100

    Gardening (accname-test)

--- test/src/test-name.js
+++ test/src/test-name.js
@@ -2,19 +2,18 @@ var accnameTest = require('accname-test/dist/accname-test');
 
 describe('getName / getDescription', function() {
 	Object.keys(accnameTest.files).forEach(function(name) {
-		var html = accnameTest.files[name];
 		var testbed = document.createElement('div');
-		testbed.innerHTML = html;
+		testbed.innerHTML = accnameTest.files[name];
+
+		before(function() {
+			document.body.appendChild(testbed);
+		});
+
+		after(function() {
+			document.body.removeChild(testbed);
+		});
 
 		describe(name, function() {
-			before(function() {
-				document.body.appendChild(testbed);
-			});
-
-			after(function() {
-				document.body.removeChild(testbed);
-			});
-
 			accnameTest.getTests(testbed).forEach(function(test) {
 				it(test.comment || name, function() {
 					if (test.name !== null) {
