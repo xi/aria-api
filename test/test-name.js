91d1a4aa54f7ed517078e09ed08289fcfb22bea3
Tobias Bengfort <tobias.bengfort@posteo.de>
2024-02-02 16:38:49 +0100

    wpt: split wpt tests

--- test/test-name.js
+++ test/test-name.js
@@ -1,9 +1,5 @@
 describe('getName / getDescription', () => {
 	var testbed = document.createElement('div');
-	var known_failing = [
-		'Name test case 660',
-		'Name test case 659',
-	];
 
 	before(() => {
 		document.body.appendChild(testbed);
@@ -11,25 +7,6 @@ describe('getName / getDescription', () => {
 
 	after(() => {
 		document.body.removeChild(testbed);
-	});
-
-	window.wpt.accname.forEach(test => {
-		var _it = known_failing.includes(test.title) ? xit : it;
-		_it(test.title, () => {
-			testbed.innerHTML = test.html;
-			for (var element of document.querySelectorAll(test.selector)) {
-				var name = element.dataset.expectedlabel ?? test.name;
-				var description = element.dataset.expecteddescription ?? test.description;
-				expect(name ?? description).toNotBe(null);
-
-				if (name !== null) {
-					expect(aria.getName(element)).toBe(name);
-				}
-				if (description !== null) {
-					expect(aria.getDescription(element)).toBe(description);
-				}
-			}
-		});
 	});
 
 	it('ignores <noscript>', () => {
