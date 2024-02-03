3d3015b77e497494e3a380836a60171bbfc124c0
Tobias Bengfort <tobias.bengfort@posteo.de>
2024-02-03 10:33:31 +0100

    wpt: extract subtests

--- test/test-wpt.js
+++ test/test-wpt.js
@@ -1,3 +1,5 @@
+/* global describe, it, xit, before, after, expect, aria */
+
 describe('wpt', () => {
 	var testbed = document.createElement('div');
 	var known_failing = [
@@ -5,33 +7,74 @@ describe('wpt', () => {
 		'Name test case 659',
 	];
 
+	var withId = function(element, fn) {
+		if (element.id) {
+			fn();
+		} else {
+			element.id = 'subtest';
+			fn();
+			element.removeAttribute('id');
+		}
+	};
+
+	var extractTests = function(data) {
+		var tests = [];
+		document.body.appendChild(testbed);
+		testbed.innerHTML = data.html;
+		for (var element of testbed.querySelectorAll(data.selector)) {
+			withId(element, () => {
+				tests.push({
+					'filename': data.filename,
+					'title': element.dataset.testname || data.title,
+					'html': testbed.innerHTML,
+					'name': element.dataset.expectedlabel,
+					'description': element.dataset.expecteddescription,
+					'role': element.dataset.expectedrole,
+					'selector': `#${element.id}`,
+				});
+			});
+		}
+		testbed.remove();
+		return tests;
+	};
+
+	var registerTest = function(test) {
+		var _it = known_failing.includes(test.title) ? xit : it;
+		_it(test.title, () => {
+			testbed.innerHTML = test.html;
+			expect(test.name != null || test.description != null || test.role != null).toBe(true);
+			var element = testbed.querySelector(test.selector);
+
+			if (test.name != null) {
+				expect(aria.getName(element)).toBe(test.name);
+			}
+			if (test.description != null) {
+				expect(aria.getDescription(element)).toBe(test.description);
+			}
+			if (test.role != null) {
+				expect(aria.getRole(element)).toBe(test.role);
+			}
+		});
+	};
+
 	before(() => {
 		document.body.appendChild(testbed);
 	});
 
 	after(() => {
-		document.body.removeChild(testbed);
+		testbed.remove();
 	});
 
 	for (const key in window.wpt) {
 		describe(key, () => {
 			window.wpt[key].forEach(test => {
-				var _it = known_failing.includes(test.title) ? xit : it;
-				_it(test.title, () => {
-					testbed.innerHTML = test.html;
-					for (var element of document.querySelectorAll(test.selector)) {
-						var name = element.dataset.expectedlabel ?? test.name ?? null;
-						var description = element.dataset.expecteddescription ?? test.description ?? null;
-						expect(name ?? description).toNotBe(null);
-
-						if (name !== null) {
-							expect(aria.getName(element)).toBe(name);
-						}
-						if (description !== null) {
-							expect(aria.getDescription(element)).toBe(description);
-						}
-					}
-				});
+				if (test.selector.startsWith('#')) {
+					registerTest(test);
+				} else {
+					describe(test.title, () => {
+						extractTests(test).forEach(registerTest);
+					});
+				}
 			});
 		});
 	}
