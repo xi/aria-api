5fc3becd1d2d93998edba20b18b7fa3088f0e944
Tobias Bengfort <tobias.bengfort@posteo.de>
2024-11-01 12:39:57 +0100

    refactor: do not re-export methods

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var query = require('./lib/query.js');
+var attrs = require('./lib/attrs.js');
 var name = require('./lib/name.js');
 var atree = require('./lib/atree.js');
 
 module.exports = {
-	getRole: query.getRole,
-	getAttribute: query.getAttribute,
+	getRole: attrs.getRole,
+	getAttribute: attrs.getAttribute,
 	getName: name.getName,
 	getDescription: name.getDescription,
 
